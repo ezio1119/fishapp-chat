873e11a6fce909eecdc9dcd82919d262c1b8c40e
Katsutoshi Hayashi <ezio1119@gmail.com>
2020-06-22 15:41:39 +0900

    add

--- infrastructure/grpc.go
+++ infrastructure/grpc.go
@@ -1,10 +1,15 @@
 package infrastructure
 
 import (
+	"context"
+
 	"github.com/ezio1119/fishapp-chat/infrastructure/middleware"
 	"github.com/ezio1119/fishapp-chat/pb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 func NewGrpcServer(middLe *middleware.Middleware, chatController pb.ChatServiceServer) *grpc.Server {
@@ -22,6 +27,17 @@ func NewGrpcServer(middLe *middleware.Middleware, chatController pb.ChatServiceS
 	)
 
 	pb.RegisterChatServiceServer(server, chatController)
+	grpc_health_v1.RegisterHealthServer(server, &healthHandler{})
 	reflection.Register(server)
 	return server
+}
+
+type healthHandler struct{}
+
+func (*healthHandler) Check(ctx context.Context, in *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
+	return &grpc_health_v1.HealthCheckResponse{Status: grpc_health_v1.HealthCheckResponse_SERVING}, nil
+}
+
+func (*healthHandler) Watch(in *grpc_health_v1.HealthCheckRequest, s grpc_health_v1.Health_WatchServer) error {
+	return status.Error(codes.Unimplemented, "watch is not implemented.")
 }
